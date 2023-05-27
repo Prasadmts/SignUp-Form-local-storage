c59241e801dfb644a3f9b0ecd18cc67426889d7f
Prasadmts <124504772+Prasadmts@users.noreply.github.com>
2023-05-27 23:28:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,143 @@
+function checkAccessToken() {
+    var currentUrl = window.location.href;
+    
+    if (currentUrl.indexOf('index.html') !== -1 && localStorage.getItem('token')) {
+      window.location.href = 'profile.html'; 
+    } else if (currentUrl.indexOf('profile.html') !== -1 && !localStorage.getItem('token')) {
+        window.location.href = 'index.html'; 
+    }
+ }
+        
+        
+        
+// Check access token on page load
+window.onload = function() {
+checkAccessToken();
+};
+
+
+function validateForm(event) {
+    event.preventDefault(); 
+    
+    
+    var fullname = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('pass').value;
+    var confirmPassword = document.getElementById('confirm-pass').value;
+    var errorSpan = document.getElementById('error-span');
+    var errDiv= document.getElementById('error-div');
+    var SuccessDiv= document.getElementById('successfully-div');
+    var logout= document.getElementById('logout');
+    
+    
+    if (fullname === '' || email === '' || password === '' || confirmPassword === '') {
+    //   alert('Please fill in all fields.');
+      errDiv.style.display="block";    
+      errorSpan.innerHTML=`All the fields are mandatory `;
+      setTimeout(() => {
+        errDiv.style.display="none";
+      }, 5000);
+      
+      return; 
+    }
+    
+    
+    if (password !== confirmPassword) {
+    //   alert('Passwords do not match.');
+      
+      errDiv.style.display="block";    
+      errorSpan.innerHTML=`Passwords do not match.`;
+      setTimeout(() => {
+        errDiv.style.display="none";
+      }, 5000);
+      return; 
+    }
+    
+   
+    console.log('Full Name:', fullname);
+    console.log('Email:', email);
+    console.log('Password:', password);
+    console.log('Confirm Password:', confirmPassword);
+    
+    
+    document.getElementById('form').reset();
+    SuccessDiv.style.display="block";    
+    SuccessDiv.innerHTML=`Successfully Signed Up!`;
+      setTimeout(() => {
+        SuccessDiv.style.display="none";
+      }, 5000);
+
+      
+    var randomToken = generateRandomToken();    
+    console.log(randomToken);
+    saveDataToLocalStorage("name",fullname);
+    saveDataToLocalStorage("email",email);
+    saveDataToLocalStorage("password",password);
+    saveDataToLocalStorage("token",randomToken);
+
+    loadUrl("./profile.html");
+    
+  }
+
+  
+
+  function loadUrl(urlThis){
+    setTimeout(() => {
+        window.location.href=urlThis;
+    }, 3000);
+    
+  }
+var currentUrl = window.location.href;
+if (currentUrl.indexOf('index.html') !== -1 && localStorage.getItem('token')) {
+    showDataInDiv(); 
+  }
+
+//   logout
+logout.addEventListener("click",()=>{
+    
+    localStorage.removeItem("name");
+    localStorage.removeItem("email");
+    localStorage.removeItem("password");
+    localStorage.removeItem("token");
+    loadUrl("./index.html");
+})
+    
+
+
+
+  function saveDataToLocalStorage(key, value) {
+    localStorage.setItem(key, value);
+  }
+  
+  // Retrieving data from local storage
+  function getDataFromLocalStorage(key) {
+    var value = localStorage.getItem(key);
+    return value ? value : null;
+  }
+  
+
+  function showDataInDiv(){
+
+    var nameShow = getDataFromLocalStorage("name")
+    var emailShow = getDataFromLocalStorage("email")
+    var passShow = getDataFromLocalStorage("password")
+    // getDataFromLocalStorage("token")
+    var div = `<div>Full Name : <span id="show-name">${nameShow}</span></div>
+    <div>Email : <span id="show-email">${emailShow}</span></div>
+    <div>Password : <span id="show-password">${passShow}</span></div>` ;
+    var showAllData = document.getElementById('show-all-data');
+    showAllData.innerHTML=div;
+  }
+
+
+  function generateRandomToken() {
+    var randomBytes = new Uint8Array(16);
+    crypto.getRandomValues(randomBytes);
+    
+    var token = Array.from(randomBytes, function(byte) {
+      return ('0' + byte.toString(16)).slice(-2);
+    }).join('');
+    
+    return token;
+  }
+  
